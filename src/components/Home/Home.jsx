3a01944ded211c467960baa7a86669d8548aaa58
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2023-07-25 15:31:58 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
                     >
                         <Heading
                             color={'#1fb299'}
-                            fontSize={['5xl', '5xl', '6xl', '8xl']} className='heading'
+                            fontSize={['4xl', '5xl', '6xl', '8xl']} className='heading'
                             as={motion.h1}
                             initial={{
                                 y: '-100%',
