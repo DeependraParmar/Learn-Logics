58f6669c08dfe9a54b27e7f32b18b7c1440b352c
DeependraParmar <deependraparmar1@gmail.com>
2023-07-27 12:51:27 +0530

    Profile Page Completed

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,18 @@
-import { Avatar, Button, Container, HStack, Heading, Image, Stack, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
-import { AiOutlineUser, AiOutlineMail, AiFillEdit, AiOutlineSwap } from "react-icons/ai";
+import { Avatar, Button, Container, HStack, Heading, Image, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Stack, Text, VStack, useDisclosure } from '@chakra-ui/react'
+import React, { useEffect } from 'react'
+import { AiOutlineUser, AiOutlineMail, AiFillEdit, AiOutlineSwap, AiFillSave } from "react-icons/ai";
 import { CgCalendarDates } from "react-icons/cg";
 import { MdSubscriptions } from "react-icons/md";
 import { FcCancel } from "react-icons/fc";
 import { BsFillPlayFill } from "react-icons/bs";
 import { Link } from 'react-router-dom';
+import { RiDeleteBin7Fill } from 'react-icons/ri';
+import { fileUploadCSS } from '../Auth/Register/Register';
+import { useState } from 'react';
+import Particles from '../Home/Particles';
+
+
+
 
 const Profile = () => {
     const user = {
@@ -24,15 +31,31 @@ const Profile = () => {
             }
         ]
     }
+
+    const removeFromPlaylistHandler = (courseid) => {
+        console.log(courseid);
+    }
+    const changeImageSubmitHandler = (e,image) => {
+        e.preventDefault();
+        console.log(image);
+    }
+
+    const {isOpen,onClose,onOpen} = useDisclosure();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    }, []);
     return (
+
         <>
-            <Container maxWidth={'container.sm'} paddingTop={'7rem'} paddingX={'5'} paddingBottom={'3rem'}  >
+        <Particles />
+            <Container backdropFilter={'blur(5px)'} maxWidth={'container.sm'} paddingTop={'7rem'} paddingX={'5'} paddingBottom={'3rem'}  >
                 <Heading textAlign={'center'} fontSize={['4xl', '5xl', '5xl', '5xl']} >Profile</Heading>
                 <Stack paddingY={'3rem'} justifyContent={'flex-start'} direction={['column', 'column', 'row', 'row']} alignItems={['center']} spacing={['8', '10', '12', '16']} >
 
                     <VStack spacing={'4'} >
                         <Avatar boxSize={'40'} />
-                        <Button colorScheme={'teal'} variant={'ghost'}>Change Profile</Button>
+                        <Button onClick={onOpen} colorScheme={'teal'} variant={'ghost'}>Change Profile</Button>
                     </VStack>
                     <VStack spacing={'4'} justifyContent={'flex-start'} alignItems={['flex-start', 'flex-start']} >
                         <HStack >
@@ -83,9 +106,9 @@ const Profile = () => {
                 <Heading size={'md'} my={'8'} textAlign={'center'} >Your Courses</Heading>
                 {
                     user.playlist.length > 0 ? (
-                        <Stack direction={['column', 'column', 'row', 'row']} spacing={'4'} justifyContent={'center'} alignItems={'center'} flexWrap={'wrap'} >
+                        <Stack direction={['column', 'column', 'row', 'row']} spacing={'4'} justifyContent={'flex-start'} alignItems={'center'} flexWrap={'wrap'} >
                             {
-                                user.playlist.map((course, index) => (
+                                user.playlist.map((course) => (
                                     <VStack alignItems={'flex-start'} width={'200px'} key={course.courseid} spacing={'4'} >
                                         <Image src={course.posterURL} alt={course.title} width={'full'} objectFit={'contain'} />
                                         <Text textAlign={'center'} >{course.title}</Text>
@@ -93,6 +116,7 @@ const Profile = () => {
                                             <Link to={`/course/${course.courseid}`} >
                                                 <Button colorScheme={'teal'} variant={'solid'} size={'sm'} gap={'2'} ><BsFillPlayFill size={'20'} />View Course</Button>
                                             </Link>
+                                            <Button onClick={() => removeFromPlaylistHandler(course.courseid)} variant={'outline'} colorScheme='teal' size={'sm'} ><RiDeleteBin7Fill size={'16'} /></Button>
                                         </HStack>
                                     </VStack>
                                 ))
@@ -103,10 +127,56 @@ const Profile = () => {
                     )
                 }
 
+                <ChangeProfilePhoto isOpen={isOpen} onClose={onClose} changeImageSubmitHandler={changeImageSubmitHandler} />
+
             </Container>
 
         </>
     )
 }
 
-export default Profile
+export default Profile;
+
+function ChangeProfilePhoto({isOpen,onClose,changeImageSubmitHandler}) {
+    const [imagePrev, setImagePrev] = useState('');
+    const [image, setImage] = useState('');
+
+    const changeImageHandler = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setImagePrev(reader.result);
+            setImage(file);
+        }
+    }
+    const closeHandler = () => {
+        onClose();
+        setImagePrev('');
+        setImage('');
+    }
+    return (
+        <Modal isOpen={isOpen} onClose={closeHandler} >
+            <ModalOverlay backdropFilter={'blur(5px)'} />
+            <ModalContent>
+                <ModalHeader ><Text textAlign={'center'}>Change Profile Picture</Text></ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Container maxWidth={'container.sm'} >
+                        <form onSubmit={(e)=> changeImageSubmitHandler(e,image)} >
+                            <VStack spacing={'8'}>
+                                {imagePrev && <Avatar src={imagePrev} boxSize={'40'} />}
+                                <Input onChange={changeImageHandler} type={'file'} css={{ "&::file-selector-button": fileUploadCSS }} />
+
+                                <Button type='submit' colorScheme={'teal'} variant={'solid'} gap={'2'} ><AiFillSave size={'20'} /> Save Changes</Button>
+                            </VStack>
+                        </form>
+                    </Container>
+                </ModalBody>
+                <ModalFooter>
+                    <Button onClick={closeHandler} colorScheme={'teal'} variant={'ghost'} >Cancel</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
