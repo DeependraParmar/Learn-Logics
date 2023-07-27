c1f541dd22a356276af1356e40ff41cbf8088557
DeependraParmar <deependraparmar1@gmail.com>
2023-07-27 09:27:37 +0530

    404 Not Found Page Ready

--- src/components/Layout/Not Found/NotFound.jsx
+++ src/components/Layout/Not Found/NotFound.jsx
@@ -1,10 +1,17 @@
+import { Button, Container, Heading, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import image from '../../../assets/images/404 not found.avif'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <Container maxW={'container.xl'} paddingTop={'7rem'} paddingBottom={'3rem'}  >
+          <Image src={image} filter={'saturation(120%)'} alt="404" margin={'auto'} />
+        <Heading textAlign={'center'} textShadow={'lg'} fontSize={['xl', '2xl', '4xl', '6xl']} paddingY={'2rem'} >Page Not Found😓</Heading>
+        <Text textAlign={'center'} >I think you lost somewhere else. Go to <Link to='/'><Button variant={'link'} colorScheme='teal' >Home Page</Button></Link></Text>
+        </Container>
+    </>
   )
 }
 
